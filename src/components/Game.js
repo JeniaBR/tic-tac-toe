126c7355547665f8c90562e2baa04f341fed5074
Jenia Brook <jeniabrook@gmail.com>
2017-05-02 20:29:00 +0300

    Add logic to game

--- src/components/Game.js
+++ src/components/Game.js
@@ -17,8 +17,59 @@ class Game extends Component {
     }
   }
 
+  calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+
+    return null;
+}
+
   handleTileClick = (location, player) => {
-    console.log('Clicked on TILE',location);
+    if(this.state.gameBoard[location] === 'X' || this.state.gameBoard[location] === 'O' || this.state.winner){
+      //This tile is taken already
+      return;
+    }
+    const currentGameBoard = this.state.gameBoard.slice();
+    currentGameBoard[location] = this.state.turn;
+    this.setState({
+      gameBoard: currentGameBoard,
+      turn: this.state.turn === 'X' ? 'O' : 'X'
+    });
+
+    const winner = this.calculateWinner(currentGameBoard);
+
+    if(winner){
+      this.setState({
+        winner: this.state.turn
+      })
+    }
+
+    const moves = currentGameBoard.join('').replace(/ /g, '');
+    if(moves.length === 9){
+      this.setState({
+        winner: 'd'
+      });
+    }
+
+    // this.setState({
+    //   turn: this.state.turn === 'X' ? 'O' : 'X'
+    // })
+
   }
 
   resetBoard = () => {
